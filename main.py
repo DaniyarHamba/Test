82ff5ea902176bb32e8d08bba8584f3272c27692
DaniyarHamba <144147690+DaniyarHamba@users.noreply.github.com>
2023-09-05 21:20:50 +0600

    Lambda
    Description

---
+++ main.py
@@ -0,0 +1,11 @@
+# a = [(lambda x: x**2)(i) for i in range(100)]
+# print(a)
+
+# cl_showfps true
+# cl_righthand 0
+# sv_cheats true
+# r_drawothermodels true
+# bot_kick
+# bind l noclip
+# i need bullets
+
